a7513466dd331603955adc9804ed43cb4b40d9a2
Diana Ivanova <diana.yastrebova02@mail.ru>
2023-08-08 12:46:36 +0300

    settings

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,34 @@
-import React from 'react';
-import './App.css';
+import { Route, Routes } from 'react-router';
+import styles from './App.module.scss';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Cartoons from './pages/Cartoons';
+import Films from './pages/Films';
+import Home from './pages/Home';
+import Movie from './pages/Movie';
+import NotFound from './pages/NotFound';
+import Person from './pages/Person';
+import Series from './pages/Series';
 
 function App() {
+
+
   return (
-    <div className="App">
+    <div className={styles.app}>
+      <Header/>
+      <main className={styles.main}>
+      <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/cartoons' element={<Cartoons/>}/>
+          <Route path='/films' element={<Films/>}/>
+          <Route path='/series' element={<Series/>}/>
+          <Route path='/movie/:id' element={<Movie/>}/>
+          <Route path='/person' element={<Person/>}/>
+
+          <Route path="*" element={<NotFound/>}/>
+      </Routes>
+      </main>
+      <Footer/>
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,25 @@
+import './index.scss';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
+
+import {BrowserRouter} from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { store, persistor } from './redux/store';
+import { PersistGate } from 'redux-persist/integration/react';
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
+  <BrowserRouter>
+  <Provider store={store}>
+  <PersistGate loading={'loader'} persistor={persistor}>
   <React.StrictMode>
     <App />
   </React.StrictMode>
+  </PersistGate>
+  </Provider>
+  </BrowserRouter>
 );
